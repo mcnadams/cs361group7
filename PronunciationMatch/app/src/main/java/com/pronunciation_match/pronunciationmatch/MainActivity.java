bae0e74bcf058931efee25d659e5e1a996181690
mcnadams <mcneilbo@oregonstate.edu>
2018-11-17 19:04:48 -0800

    first commit

--- PronunciationMatch/app/src/main/java/com/pronunciation_match/pronunciationmatch/MainActivity.java
+++ PronunciationMatch/app/src/main/java/com/pronunciation_match/pronunciationmatch/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v("Hello world!");
         setContentView(R.layout.activity_main);
     }
 }
